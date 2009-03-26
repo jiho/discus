bc07e9149255ff5b8f9cc452dd6a21e471b0c368
Jean-Olivier Irisson <irisson@normalesup.org>
2009-03-26 15:23:29 -0400

    Improve image re-organisation
    - create one image sequence per folder instead of one big sequence (still not sure about that)
    - use tr instead of echo for character replacement, because it is more flexible

--- reorg_data.sh
+++ reorg_data.sh
@@ -1,21 +1,40 @@
 #!/bin/sh
 #
-#	Copy images from the folder hierarchy to a large sequence 
+#	Reorganise images in the folder hierachy
 #
+#	Images are stored in a 'DCIM'  directory per day, with a directory structure and names imposed by the camera. Here we extract the images from this complex hierarchy into one sequence of images per day, with names indicating the time the picture was taken.
+#
+#	(c) Copyright 2009 Jean-Olivier Irisson. GNU General Public License
+#
+#------------------------------------------------------------------------------
 
-find . -name "DSC*.JPG" | while read file
-do
-	# echo $file
+
+prefix="../Lizard_Island/data/DISC"
+
+for folder in $(echo ${prefix}/200*); do
+
+	echo "$folder"
+
+	dataSource=${folder}"/DCIM"
+	dataDestination=${folder}"/DCIM-all"
 	
-	folderNumber=$(echo $file | awk -F "NCD" {'print $1'} | awk -F "/" {'print $NF'})
-	# echo $folderNumber
+	# create destination directory if it does not exists
+	mkdir -p $dataDestination
 	
-	fileNumber=$(echo $file | awk -F "_" {'print $NF'})
-	# echo $fileNumber
+	# for each picture
+	find $dataSource -name "DSC*.JPG" | while read file
+	do
+		# get date and time of shoot, to the centi-second, using exif tool
+		dateTime=$(exiftool -p '${CreateDate}.${SubSecTime}' $file)
+		# replace spaces to make it easier to deal with the filename
+		newName=$(echo ${dateTime} | tr " " "_" | tr ":" "-")".jpg"
+		# echo $(echo $file | awk -F "/" {'print $NF'}) "->" $newName
 	
-	newName=${folderNumber}${fileNumber}
-	# echo $newName
-	ln $file $newName
+		# create a hard link in the destination directory
+		ln $file $dataDestination/$newName
+		# when two files have exactly the same time stamp, only, they would end up with the same name but the first one is in fact not overwritten.
+	done
+
 done
 
 exit 0
