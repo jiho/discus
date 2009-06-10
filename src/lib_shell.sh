41c222aa8db92b716d112b446caeb4afefc759a0
Jean-Olivier Irisson <irisson@normalesup.org>
2009-06-10 15:38:49 -0400

    Change syntax for cross platform compatibility
    FreeBSD sed (=OS X sed) and GNU sed behave differently

--- src/lib_shell.sh
+++ src/lib_shell.sh
@@ -169,7 +169,7 @@ write_pref() {
 
 	if [[ $? == "0"  ]]; then
 		# if there is a match, update the preference in the config file
-		sed -e 's/'$pref'=.*/'$pref'='\"$(eval echo \$$pref)\"'/' -i '' $configFile
+		sed -e 's/'$pref'=.*/'$pref'='\"$(eval echo \$$pref)\"'/' -i='' $configFile
 	else
 		# otherwise, write the pref at the end of the file
 		echo "$pref=\"$(eval echo \$$pref)\"" >> $configFile
